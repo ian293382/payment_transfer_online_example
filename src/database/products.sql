f8882d570c09e999de8a221489d04ff846ea49a5
Iannaan <moshou5571228@gmail.com>
2024-03-24 01:47:36 +0800

    ADD COLUMN  INT UNSIGNED NOT NULL DEFAULT '0' AFTER ' '

--- src/database/products.sql
+++ src/database/products.sql
@@ -1,7 +1,10 @@
-CREATE TABLE `product` (
+CREATE TABLE `products` (
     `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT  PRIMARY KEY,
     `name` varchar(255) NOT NULL DEFAULT '',
     `amount` int UNSIGNED NOT NULL DEFAULT 0,
     `description` text, 
     `pre_order` int UNSIGNED NOT NULL DEFAULT 0,
+    `price` int UNSIGNED NOT NULL DEFAULT 0
 );
+
+ADD COLUMN `price` INT UNSIGNED NOT NULL DEFAULT '0' AFTER `pre_order`, RENAME TO  `node_js_3rd_pay`.`products` ;
