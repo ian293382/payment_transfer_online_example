64d2314ac412ad4ea3a08cac191f5d796d986502
Iannaan <moshou5571228@gmail.com>
2024-03-24 04:06:37 +0800

    setting database  Knex.jx dotenv package

---
+++ src/utils/index.ts
@@ -0,0 +1,16 @@
+import knex, { Knex } from "knex";
+
+export const createDatabase = () => {
+    return knex({
+            client: 'mysql',
+            version: '5.7',
+            connection: {
+              host: process.env.DATABASE_HOST || '127.0.0.1',
+              port: Number(process.env.DATABASE_PORT) || 3306,
+              user: process.env.DATABASE_USER || 'root',
+              password: process.env.DATABASE_PASSWORD || '',
+              database: process.env.DATABASE_DATABASE || 'node_js_3rd_pay',
+            },
+            pool: {min: 2, max: 10},
+    })
+}
