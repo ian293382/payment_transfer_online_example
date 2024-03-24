795a7cf5ab115363a7c066286d87a9710acd4a39
Iannaan <moshou5571228@gmail.com>
2024-03-25 03:42:30 +0800

    useing vue3 to bind api for localhost:3000

--- src/public/javascripts/index.js
+++ src/public/javascripts/index.js
@@ -1,3 +1,15 @@
 const serverDomain = 'http://localhost:3000';
 
-var vue = new Vue()
+const app = Vue.createApp({
+    data() {
+        return {
+            products: [],
+        };
+    },
+    async mounted() {
+        this.products = await fetch(`${serverDomain}/products/list`).then((res) => res.json());
+    },
+});
+
+// 將 Vue app 掛載到指定的元素上
+app.mount('#app');
