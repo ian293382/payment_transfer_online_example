5927a0f1a6c4dcafc1293cdca959b46e3db6a6c7
Iannaan <moshou5571228@gmail.com>
2024-03-25 03:01:14 +0800

    try connect frontend is success

---
+++ src/public/javascripts/index.js
@@ -0,0 +1,3 @@
+const serverDomain = 'http://localhost:3000';
+
+var vue = new Vue()
---
+++ src/routes/product.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { ControllerContext } from "@/manager/controllerManager";
+
+export const mountProductRouter = ({
+    controllerCtx,
+}: {
+    controllerCtx: ControllerContext;
+}) => {
+
+    let router = express.Router();
+
+    router.get('/list', controllerCtx.ProductController.findAll);
+
+    return router;
+}
