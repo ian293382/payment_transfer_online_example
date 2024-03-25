ef41f36aa7e4c65a49fb7ac37b33d6c848e02395
Iannaan <moshou5571228@gmail.com>
2024-03-25 12:20:55 +0800

    create order model

--- src/model/order.ts
+++ src/model/order.ts
@@ -1,3 +1,4 @@
+import { Knex } from "knex";
 import { Base } from "./base";
 
 // 處理 enum
@@ -25,17 +26,46 @@ export interface OrderContents {
 }
 
 // 先用介面將資料設定出來 改寫成 Ts 用的 enum 需要另外建立 引入
+// 記得這邊要用駝峰狀
 export interface Order {
     id: string; 
     total: number;
-    created_at: Date;
-    updated_at: Date;
-    payment_provider: PaymentProvider;
-    payment_pay: PaymentWay;
+    createdAt: Date;
+    updatedAt: Date;
+    paymentProvider: PaymentProvider;
+    paymentPay: PaymentWay;
     status: OrderStatus;
     contents: OrderContents[];
 }
 
 export class OrderModel extends Base<Order> {
+    tableName = 'orders';
+    // schema 要對應資料庫裡面的格式 title 例如 `id` 
+    schema = {
+        id: `id`,    
+        total: `total`,
+        created_at: `created_at`,
+        updated_at: `updated_at`,
+        payment_provider: `payment_provider`,
+        payment_pay: `payment_pay`,
+        status: `status`,
+        contents: `contents`,
+    };
 
+    static createModel = ({
+        knexSql,
+        tableName,
+    }: {
+        knexSql: Knex;
+        tableName?: string;
+    }) => {
+        return new OrderModel({
+            knexSql,
+            tableName,
+        });
+    }
+
+    constructor({ knexSql,tableName,}: { knexSql: Knex; tableName?: string }) {
+        super({ knexSql, tableName });
+    }
 }
