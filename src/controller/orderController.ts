0e368728dde79e4c4e8b6658daa884dfef12180c
Iannaan <moshou5571228@gmail.com>
2024-03-27 23:56:33 +0800

    implement express validator middleware in createOrder

--- src/controller/orderController.ts
+++ src/controller/orderController.ts
@@ -4,7 +4,9 @@
 import { IOrderModel, OrderContents, PaymentPay, PaymentProvider } from "@/model/order";
 import { IProductModel } from "@/model/product";
 import { NextFunction, Request, Response } from "express";
+import { ValidationChain, body, validationResult } from "express-validator";
 import { Knex } from "knex";
+import { isEmpty } from "lodash";
 
 interface CreateOrderRequestParams {
     paymentProvider: PaymentProvider;
@@ -16,6 +18,7 @@ interface CreateOrderRequestParams {
 // 一樣 因為是根據路由訂單 從 router 去看 req摻數貼上 把數值都改成any 稍後調整我們所需求的 params
 // void 是因為這些函數會強制回傳 ( render response json ) 我們不需要 他回傳任何值 ( 抽象 )
 export interface IOrderController {
+    createOrderValidator(): ValidationChain[];
     createOrder(
         req: Request<any, any, CreateOrderRequestParams, any>,
         res: Response,
@@ -59,6 +62,46 @@ export class OrderController implements IOrderController {
         this. orderModel = orderModel;
         this.productModel = productModel
     }
+    // 放在route 為了交給後台驗證
+    public createOrderValidator = () => {
+
+        const paymentProviderValidator =(value :any) =>{
+            return [PaymentProvider.ECPAY,PaymentProvider.PAYPAL].includes(value); 
+        }
+
+        const paymentPayValidator =(value :any) =>{
+            return [PaymentPay.CVS,PaymentPay.PAYPAL].includes(value); 
+        }
+
+        const contentValidator =(value :any) =>{
+            if (isEmpty(value)) return false;
+
+
+            for (const product of value) {
+                if (
+                   [product.productId, product.amount, product.price.some].some((value) => !value) 
+                ) 
+                    return false;
+            }
+        }
+
+        // 多個資料 就用 array 去設定驗證資料 驗證
+        return  [
+            body('paymentProvider', 'Invalid payment provider').custom(
+                paymentProviderValidator
+            ),
+            body('paymentPay', 'Invalid payment way ').custom(
+                paymentPayValidator
+            ),
+            body('contents', 'Invalid payment content')
+            .isArray()
+            .custom(
+                contentValidator
+            ),
+
+        ];
+        
+    };
     // 實現功能 沒用到next 故意先加一個底線
     public createOrder: IOrderController["createOrder"] = (req, res, _next) => {
         // 倉品id, 商品名稱, 商品數量 => contents === [ ] post, payment_provider, payment_pay 
@@ -68,16 +111,26 @@ export class OrderController implements IOrderController {
             paymentProvider,
             paymentPay,
             contents);
-        res.json({ status: 'success' });
+       
 
-        // 資料驗證 
-        // 驗證格式
+        // 資料驗證  使用 express-validator  因為不論怎樣都回傳通過 但實際上失敗不會紀錄
+        // 驗證格式 .validationResult() https://express-validator.github.io/docs/api/express-validator/#validationresult
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array(),
+            });
+        }
+
+        // 實際處理
+            
+        
         // 2, 寫入database
         // 3. 金流api
-        
+        res.json({ status: 'success' });
     };
 
-    public updateAmount: IOrderController["updateAmount"] = (req, res, next) => {
+    public updateAmount: IOrderController["updateAmount"] = (req, res, _next) => {
         // TODO: Implement your updateAmount logic here
     
         // For example:
--- src/routes/order.ts
+++ src/routes/order.ts
@@ -8,7 +8,11 @@ export const mountOrderRouter = ({
 }) => {
     let router = express.Router();
 
-    router.post("/create", controllerCtx.orderController.createOrder);
+    router.post("/create",
+    // 俗稱middleware 中介層 createOrderValidator 是函數 createOrderValidator()
+    controllerCtx.orderController.createOrderValidator(),
+    // controller create Order 完整內容
+    controllerCtx.orderController.createOrder);
 
     return router;
 }
