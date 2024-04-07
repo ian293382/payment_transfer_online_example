8f68d1a73f9ddf698b97a2cc619ef8e2fe784159
Iannaan <moshou5571228@gmail.com>
2024-04-07 14:48:08 +0800

    Vue install and setting in index.js

---
+++ src/public/javascripts/index.js
@@ -0,0 +1,16 @@
+// 先寫死
+const serverDomain = 'http://localhost:3000';
+
+const app = Vue.createApp({
+    data() {
+        return {
+            products: [],
+        };
+    },
+    async mounted() {
+        this.products = await fetch(`${serverDomain}/products/list`).then((res) => res.json());
+    },
+});
+
+// 將 Vue app 掛載到指定的元素上
+app.mount('#app');
