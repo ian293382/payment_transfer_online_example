0d95cbe718c698532627491092f4a0b8ddc60e41
Iannaan <moshou5571228@gmail.com>
2024-04-07 19:05:15 +0800

    implement createOrder model

---
+++ src/model/order.ts
@@ -0,0 +1,68 @@
+import { Knex } from "knex";
+import { Base, IBase } from "./base";
+
+export enum PaymentProvider {
+    Paypal = 'Paypal',
+    ECPAY = 'ECPAY',
+}
+
+export enum PaymentPay {
+    CVS = "CVS",
+    PAYPAL = "PAYPAL",
+}
+
+export enum OrderStatus {
+    WAITING = "WAITING",
+    SUCCESS = "SUCCESS",
+    FAILED = "FAILED",
+    CANCEL = "CANCEL",
+
+}
+export interface OrderContent {
+    product: number;
+    amount: number;
+    price: number;
+}
+
+export interface Order {
+    id: string;
+    total: number;
+    created_at: Date;
+    updated_at: Date;
+    // enum 是要額外建立的方法去儲存
+    payment_provider:  PaymentProvider;
+    payment_pay: PaymentPay;
+    status:  OrderStatus;
+    contents: OrderContent[];
+}
+
+// 上述定義 下面要實作他的方法 
+export class OrderModel extends Base<Order>  implements IBase<Order> {
+    protected tableName = 'orders';
+    // 要把功能寫出來時 記得js裡面是 駝峰裝的key值 ，資料庫判別適用底線命名的 左邊 js = database
+    protected schema = {
+        id: `id`,
+        total: `total`,
+        createdAt: `created_at`,
+        updatedAt: `updated_at`,
+        paymentProvider:  `payment_provider`,
+        paymentPay: `Payment_pay`,
+        status:  `status`,
+        contents: `contents`,       
+    };
+
+    static createConstructor = ({ 
+        knexSql,
+        tableName,
+    }: { 
+        knexSql: Knex;
+        tableName: string; 
+    }) => {
+        return new OrderModel({  knexSql, tableName });
+    };
+
+    // 整理成讓knex 資料可以傳入 tableName? 可能是空的值 
+    constructor({ knexSql, tableName }: { knexSql: Knex, tableName?: string }) {
+        super({ knexSql, tableName });
+    }
+}
