0f19025dae48503dee5fcba79a33038138fef019
Iannaan <moshou5571228@gmail.com>
2024-04-07 14:47:35 +0800

    build product route find_all

---
+++ src/routes/product.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { ControllerContext } from '@/manager/controllerManager';
+
+export const mountProductRouter = ({
+    controllerCtx
+}: {
+    controllerCtx: ControllerContext;
+}) => {
+    let router = express.Router();
+
+    router.get('/list', controllerCtx.ProductController.findAll)
+
+    return router;
+};
